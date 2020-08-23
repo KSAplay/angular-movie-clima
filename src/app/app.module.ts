6b095170dcffde3caa72f5f5188204a4f9dd6be5
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2020-08-23 13:04:23 -0500

    commit on master
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviePreviewComponent } from './components/movie-preview/movie-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviePreviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/movie-preview/movie-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-preview',
+  templateUrl: './movie-preview.component.html',
+  styleUrls: ['./movie-preview.component.css']
+})
+export class MoviePreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
